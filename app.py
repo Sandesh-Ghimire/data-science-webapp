9f3615ff761111a35f18f94fbea3695d26814945
sandesh-ghimire <ghimiresandesh45@gmail.com>
2023-08-12 20:14:05 +0530

    minor changes

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ import numpy as np
 data_url=("/home/rhyme/Desktop/Project/Book1.csv")
 
 
-st.title("motor vechicle colliosions in new york city")
+st.title("Motor vechicle colliosions in new york city")
 
-st.markdown("this app is a dashboard created by the help of streamline  to analyze motor vechicle collisioon for nyc")
+st.markdown("This app is a dashboard created by the help of streamline  to analyze motor vechicle collisioon for nyc")
 
 @st.cache(persist=True)
 def load_data(nrows):
@@ -22,17 +22,17 @@ def load_data(nrows):
 data=load_data(50000)
 
 
-st.header("where are the most people injured in nyc")
-injured_people=st.slider("number of persons injured in vechicle colliosions",0.19)
+st.header("Where are the most people injured in nyc")
+injured_people=st.slider("Number of persons injured in vechicle colliosions",0.19)
 st.map(data.query("injured_persons>=@injured_people")[["latitude","longitude"]].dropna(how="any"))
 
 
 
-st.header("how many collisions occur during the given time of the day?")
-hour = st.slider("hour to look at", 0,23)
+st.header("How many collisions occur during the given time of the day?")
+hour = st.slider("Hour to look at", 0,23)
 data=data[data['date/time'].dt.hour == hour]
 
 
-if st.checkbox("show raw data", False):
-    st.subheader('raw data')
+if st.checkbox("Show raw data", False):
+    st.subheader('Raw data')
     st.write(data)
