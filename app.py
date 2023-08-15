9c5c3efe0800d6ea17130f2cc5488f72a551ead7
sandesh-ghimire <ghimiresandesh45@gmail.com>
2023-08-15 12:24:43 +0530

    Select button wasn't woking.Fixed. BUG removed.

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ import pandas as pd
 import numpy as np
 import pydeck as pdk
 import plotly.express as px
+from st_btn_select import st_btn_select
 
-data_url=("/home/rhyme/Desktop/Project/Book1.csv")
+data_url=("D:\data-science-webapp/Book1.csv")
 st.title("Motor vechicle colliosions in new york city")
 
 st.markdown("This app is a dashboard created by the help of streamline  to analyze motor vechicle collisioon for nyc")
 
-@st.cache(persist=True)
+@st.cache_data(persist=True)
 def load_data(nrows):
     data = pd.read_csv(data_url, nrows=nrows,parse_dates=[['CRASH_DATE','CRASH_TIME']])
     data.dropna(subset=['LATITUDE','LONGITUDE'],inplace=True)
@@ -59,10 +60,10 @@ layers=[
 
 st.subheader("Breakdown by minute between %i:00 and %i:00" %(hour,(hour +1)%24))
 filtered = data[
-    (data['data/time'].dt.hour>=hour)&(data['data/time'].dt.hour<(hour+1))
+    (data['date/time'].dt.hour>=hour)&(data['date/time'].dt.hour<(hour+1))
 
 ]
-hist=np.histogram(filtered['data/time'].dt.minute, bins=60 , range=(0,60))[0]
+hist=np.histogram(filtered['date/time'].dt.minute, bins=60 , range=(0,60))[0]
 
 chart_data = pd.DataFrame({'minute': range(60), 'crashes':hist})
 fig = px.bar (chart_data, x='minute', y='crashes', hover_data=['minute','crashes'], height=400)
@@ -70,21 +71,20 @@ st.write(fig)
 
 
 
+if st.checkbox("Show raw data", False):
+    st.subheader('Raw data')
+    st.write(data)
+
+
 st.header("Top 5 dangerous streets by affected people")
-select = st.selectbox('Affected type of people', hover_data=['minute','crashes'],height=400)
+select= st_btn_select(('Pedestrians','Cyclists','Motorists'))
 
 if select=='Pedestrians':
-    st.write(original_data.query("injured_pedestrains>=1")[["on_street_name","injured_pedestrains"]].sort_values(by=['injured_pedestrains'],ascending=False).dropna(how='any')[:5])
-
+    st.write(original_data.query("injured_pedestrians>=1")[["on_street_name","injured_pedestrians"]].sort_values(by=['injured_pedestrians'],ascending=False).dropna(how='any')[:5])
 
 elif select=='Cyclists':
     st.write(original_data.query("injured_cyclists>=1")[["on_street_name","injured_cyclists"]].sort_values(by=['injured_cyclists'],ascending=False).dropna(how='any')[:5])
 
-
 else:
     st.write(original_data.query("injured_motorists>=1")[["on_street_name","injured_motorists"]].sort_values(by=['injured_motorists'],ascending=False).dropna(how='any')[:5])
 
-
-if st.checkbox("Show raw data", False):
-    st.subheader('Raw data')
-    st.write(data)
